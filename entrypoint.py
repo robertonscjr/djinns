b349d5424cd5d40f290f54e4dba5b5c454db34b5
Ubuntu <ubuntu@ip-172-31-9-145.sa-east-1.compute.internal>
2021-11-16 17:18:41 +0000

    Working version of Djinns with recursive endpoints

--- entrypoint.py
+++ entrypoint.py
@@ -16,14 +16,18 @@ def view_function(*args, **kwargs):
 
     try:
         from apis import apis
-        import pdb; pdb.set_trace()
         url_rule = request.url_rule.rule
-        blueprint = "/{}".format(request.blueprint.split("/")[-1])
+        apis_keys = [a[1:] for a in apis.keys()]
+        url_rule_splitted = [a for a in url_rule.split("/") if a in apis_keys]
+        blueprint = url_rule_splitted[-1]
+        blueprint = "/" + blueprint
+
         controller_function = apis[blueprint].functions[url_rule]
         res, status = controller_function(args, kwargs, request=request)
 
     except Exception as exc:
         # TODO: log error
+        print(exc)
 
         res['error'] = True
         status = 400
